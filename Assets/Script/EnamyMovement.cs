579545501e8781f44bbc483f9c398d75cb80e214
Nattawut <Bank578974@gmail.com>
2024-04-16 19:27:23 +0700

    Add Map&EnamyTest

---
+++ Assets/Script/EnamyMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnamyMovement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Attributes")]
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Transform tartget;
+    private int pathIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tartget = LevelManager.main.path[pathIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(tartget.position,transform.position) <= 0.1f)
+        {
+            pathIndex++;
+
+            if (pathIndex >= LevelManager.main.path.Length)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            else
+            {
+                tartget = LevelManager.main.path[pathIndex];
+            }
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        Vector2 direction = (tartget.position - transform.position).normalized;
+
+        rb.velocity = direction * moveSpeed;
+    }
+}
